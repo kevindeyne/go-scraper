a7d51a28248a22a24904683c65804a6575450433
Kevin Deyne <kevindeyne@gmail.com>
2018-03-13 21:10:13 -0700

    Itch download

--- scraper.go
+++ scraper.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
-func main() {
-	c := colly.NewCollector()
+type Game struct {
+	Title       string
+	Description string
+	Url         string
+	Tags        string
+	Author      string
+}
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+func main() {
+	url := "https://itch.io/games"
+
+	c := colly.NewCollector(
+		//colly.Debugger(&debug.LogDebugger{}),
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+		colly.Async(true),
+		colly.CacheDir("./itchio_cache"),
+	)
+	detailCollector := c.Clone()
+
+	games := make([]Game, 0, 25)
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*itch.io*",
+		Parallelism: 2,
+		RandomDelay: 5 * time.Second,
+	})
+
+	c.OnHTML("a.title[href]", func(e *colly.HTMLElement) {
+		//fmt.Println("Found", e.Attr("href"))
 		e.Request.Visit(e.Attr("href"))
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+	c.OnHTML("div.formatted_description", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
 	})
 
-	c.Visit("http://go-colly.org/")
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	c.Visit(url)
+	c.Wait()
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+
+	// Dump json to the standard output
+	enc.Encode(courses)
 }
